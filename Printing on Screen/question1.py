7b459713f90ae58ef5a31ee5574d8a1b44131adc
Suraj Singh <surajsingh88597@gmail.com>
2025-06-25 09:33:01 +0530

    Printing on Screen

---
+++ Printing on Screen/question1.py
@@ -0,0 +1,9 @@
+# Write a Python program to print a formatted string using print() and f-string.
+
+def str_print():
+    name = "Suraj"
+    age = 23
+    print("Name : {} & Age : {}.".format(name, age))
+    print(f"Name : {name} & Age : {age}.")
+
+str_print()
